aa5ffcc5e6d5359eef1bd67b0c6b3dfd2b2ffd18
Valerolorenzo-m <82002695+Valerolorenzo-m@users.noreply.github.com>
2021-05-12 00:56:04 +0200

    Update Preprocessing.R

--- Preprocessing.R
+++ Preprocessing.R
@@ -4,11 +4,6 @@
 # Es recomendable ajustar los parametros segun los resultados del 
 # control de calidad
 
-# Para datos de espectrometría de masas, seleccionar el método
-# de cuantificación y los reporters:
-
-qmethod <- "NSAF" # Cambiar segun la documentacion
-rep <- reporters # A cambiar por el usuario
 
 if (type=="array"){
 
@@ -28,18 +23,9 @@ if (type=="array"){
   
 }else if(type=="MS"){
   
-  # Se define el metodo de recuento:
-   
-  data.quant <- quantify(rawdata, reporters = rep, method = qmethod) 
+  # Se filtran los NA, y se normalizan los datos
   
-  # Para operar con el paquete "DEP", en primer lugar 
-  # se convierte el MsnSet a un objeto de tipo SummarizedExperiment
-  
-  data.se <- as(data.quant, "SummarizedExperiment")
-  
-  # Se filtran los datos NA, y se normalizan los datos
-  
-  data.filt <- filter_missval(data.se, thr = 0)
+  data.filt <- filter_missval(rawdata, thr = 0)
   data <- normalize_vsn(data.filt)
 
 }
