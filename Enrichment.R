d7f80f3f6fbab44becd174f2663883e78d36238e
Valerolorenzo-m <82002695+Valerolorenzo-m@users.noreply.github.com>
2021-05-12 01:01:58 +0200

    Update Enrichment.R

--- Enrichment.R
+++ Enrichment.R
@@ -6,7 +6,7 @@ universo<- # A Definir por el usuario
 
 if (type!="MS"){
   
-  # Se lleva a cabo el análisis de enriquecimiento
+  # Se lleva a cabo el anÃ¡lisis de enriquecimiento
   genes<-data_annot
   enrich.result <- enrichPathway(gene = genes,
                                  pvalueCutoff = 0.05,
@@ -15,7 +15,7 @@ if (type!="MS"){
                                  universe = universo)
   
   # Se guardan los resultados como .CSV y un plot (cnetplot)
-  # En caso de que el archivo de enriquecimiento no esté vacío
+  # En caso de que el archivo de enriquecimiento no estÃ© vacÃ­o
   
   if (length(rownames(enrich.result@result)) != 0) {
     write.csv(as.data.frame(enrich.result), 
@@ -30,6 +30,17 @@ if (type!="MS"){
   
 }else if(type=="MS"){
   
+  enrich.result <- test_diff(data_annot, type = "manual",
+    design_formula = matriz_dis, test = contrasts)
+  
+  if (length(rownames(enrich.result@result)) != 0) {
+    write.csv(as.data.frame(enrich.result), 
+              file =paste0("./results/","DEP.Results.",contraste_int,".csv"), 
+              row.names = FALSE)
+    
+    png(file = paste0("./results/","cnetplot.",contraste_int,".png"))
+    print(cnetplot(enrich.result, vertex.label.cex = 0.75))
+    dev.off()
   
   
 }
